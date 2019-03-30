2ed12f76968bc4b8912192af0f15beee57824337
raffa <raffa@DESKTOP-J07GTV2.homenet.telecomitalia.it>
2019-03-30 19:16:14 +0100

    Nuovo branch

--- JavaApplication2/src/testpackage/MainClass.java
+++ JavaApplication2/src/testpackage/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello mondo");
-                System.out.println("Cloned");
+                System.out.println("New branch");
 	}
 
 }
