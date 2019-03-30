66cf3924fed6d0a37ef47fb53eca19dc6f9088b0
raffa <raffa@DESKTOP-J07GTV2.homenet.telecomitalia.it>
2019-03-30 19:08:26 +0100

    Progetto clonato test

---
+++ JavaApplication2/src/testpackage/MainClass.java
@@ -0,0 +1,10 @@
+package testpackage;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello mondo");
+                System.out.println("Cloned");
+	}
+
+}
